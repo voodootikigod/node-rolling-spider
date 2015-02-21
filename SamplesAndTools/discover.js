29ed05d584e3b961a1e8eb0aab7aa36dd5e2ef01
Chris Williams <chris@iterativedesigns.com>
2015-02-21 12:50:11 -0500

    Adding keyboard control example.

--- SamplesAndTools/discover.js
+++ SamplesAndTools/discover.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
-var noble = require("noble");
+var noble = require('noble');
 var knownDevices = [];
 
 noble.startScanning();
 
-noble.on("discover", function(peripheral) {
-  console.log(peripheral.advertisement.manufacturerData.toString('utf-8'));
+noble.on('discover', function(peripheral) {
+  console.log(peripheral.advertisement);
   var details = {
     name: peripheral.advertisement.localName,
     uuid: peripheral.uuid
   };
 
   knownDevices.push(details);
-  console.log(knownDevices.length + ": " + details.name + " (" + details.uuid + ")");
+  console.log(knownDevices.length + ': ' + details.name + ' (' + details.uuid + ')');
 });
