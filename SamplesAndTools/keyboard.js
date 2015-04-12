7e86d2aaafe6159559c7488b34067498e053981c
Chris Williams <voodootikigod@gmail.com>
2015-04-12 12:50:17 -0500

    Update.

--- SamplesAndTools/keyboard.js
+++ SamplesAndTools/keyboard.js
@@ -23,8 +23,12 @@ keypress(process.stdin);
 process.stdin.setRawMode(true);
 process.stdin.resume();
 
+if (process.env.UUID) {
+  console.log("Searching for ", process.env.UUID); 
+}
 
 var d = new Drone(process.env.UUID);
+
 d.connect(function () {
   d.setup(function () {
     d.flatTrim();
@@ -32,45 +36,48 @@ d.connect(function () {
     d.flatTrim();
     d.takeOff();
     d.flatTrim();
-    process.stdin.on('keypress', function (ch, key) {
-      if (ACTIVE && key) {
-        console.log(key);
-        if (key.name === 'w') {
-          d.forward({steps: STEPS});
-          cooldown();
-        } else if (key.name === 's') {
-          d.backward({steps: STEPS});
-          cooldown();
-        } else if (key.name === 'left') {
-          d.turnLeft({steps: STEPS});
-          cooldown();
-        } else if (key.name === 'a') {
-          d.tiltLeft({steps: STEPS});
-          cooldown();
-        } else if (key.name === 'd') {
-          d.tiltRight({steps: STEPS});
-          cooldown();
-        } else if (key.name === 'right') {
-          d.turnRight({steps: STEPS});
-          cooldown();
-        } else if (key.name === 'up') {
-          d.up({steps: STEPS*2.5});
-          cooldown();
-        } else if (key.name === 'down') {
-          d.down({steps: STEPS*2.5});
-          cooldown();
-        } else if (key.name === 'f') {
-          d.frontFlip({steps: STEPS});
-          cooldown();
-        } else if (key.name === 'q') {
-          d.land();
-          setTimeout(function () {
-            process.exit();
-          }, 3000);
-        } else if ( key.ctrl && key.name === 'c') {
-          process.stdin.pause();
-        }
-      }
-    });
+    
   });
+});
+
+
+process.stdin.on('keypress', function (ch, key) {
+  if (ACTIVE && key) {
+    console.log(key);
+    if (key.name === 'w') {
+      d.forward({steps: STEPS});
+      cooldown();
+    } else if (key.name === 's') {
+      d.backward({steps: STEPS});
+      cooldown();
+    } else if (key.name === 'left') {
+      d.turnLeft({steps: STEPS});
+      cooldown();
+    } else if (key.name === 'a') {
+      d.tiltLeft({steps: STEPS});
+      cooldown();
+    } else if (key.name === 'd') {
+      d.tiltRight({steps: STEPS});
+      cooldown();
+    } else if (key.name === 'right') {
+      d.turnRight({steps: STEPS});
+      cooldown();
+    } else if (key.name === 'up') {
+      d.up({steps: STEPS*2.5});
+      cooldown();
+    } else if (key.name === 'down') {
+      d.down({steps: STEPS*2.5});
+      cooldown();
+    } else if (key.name === 'f') {
+      d.frontFlip({steps: STEPS});
+      cooldown();
+    } else if (key.name === 'q') {
+      d.land();
+      setTimeout(function () {
+        process.exit();
+      }, 3000);
+    } else if ( key.ctrl && key.name === 'c') {
+      process.stdin.pause();
+    }
+  }
 });
