287f5947c6b42ee2a951f78c3094bf4afa81a229
Christopher Williams <webgeek@TikiPro.local>
2015-04-11 19:29:26 -0500

    Cooldown in the keyboad.

--- SamplesAndTools/keyboard.js
+++ SamplesAndTools/keyboard.js
@@ -3,7 +3,16 @@
 var keypress = require('keypress');
 var Drone = require('../');
 
+var ACTIVE = true;
+var STEPS = 20;
 
+
+function cooldown() {
+  ACTIVE = false;
+  setTimeout(function () {
+    ACTIVE = true;
+  }, STEPS*8);
+}
 
 // make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
@@ -24,25 +33,35 @@ d.connect(function () {
     d.takeOff();
     d.flatTrim();
     process.stdin.on('keypress', function (ch, key) {
-      if (key) {
+      if (ACTIVE && key) {
+        console.log(key);
         if (key.name === 'w') {
-          d.forward({steps: 100});
+          d.forward({steps: STEPS});
+          cooldown();
         } else if (key.name === 's') {
-          d.backward({steps: 100});
+          d.backward({steps: STEPS});
+          cooldown();
         } else if (key.name === 'left') {
-          d.turnLeft({steps: 20});
+          d.turnLeft({steps: STEPS});
+          cooldown();
         } else if (key.name === 'a') {
-          d.tiltLeft({steps: 20});
+          d.tiltLeft({steps: STEPS});
+          cooldown();
         } else if (key.name === 'd') {
-          d.tiltRight({steps: 20});
+          d.tiltRight({steps: STEPS});
+          cooldown();
         } else if (key.name === 'right') {
-          d.turnRight({steps: 20});
+          d.turnRight({steps: STEPS});
+          cooldown();
         } else if (key.name === 'up') {
-          d.up();
+          d.up({steps: STEPS*2.5});
+          cooldown();
         } else if (key.name === 'down') {
-          d.down();
+          d.down({steps: STEPS*2.5});
+          cooldown();
         } else if (key.name === 'f') {
-          d.frontFlip();
+          d.frontFlip({steps: STEPS});
+          cooldown();
         } else if (key.name === 'q') {
           d.land();
           setTimeout(function () {
