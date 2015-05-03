6a46240515c38bf6ee2492a36061098bfae5f445
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-05-03 11:42:50 -0400

    filter out non-rolling spiders and print RSSI

--- eg/discover.js
+++ eg/discover.js
@@ -6,11 +6,18 @@ var knownDevices = [];
 noble.startScanning();
 
 noble.on('discover', function(peripheral) {
+  var localName = peripheral.advertisement.localName;
+
+  if (!localName || localName.indexOf('RS_') !== 0) {
+    return; // not a rolling spider
+  }
+
   var details = {
-    name: peripheral.advertisement.localName,
-    uuid: peripheral.uuid
+    name: localName,
+    uuid: peripheral.uuid,
+    rssi: peripheral.rssi
   };
 
   knownDevices.push(details);
-  console.log(knownDevices.length + ': ' + details.name + ' (' + details.uuid + ')');
+  console.log(knownDevices.length + ': ' + details.name + ' (' + details.uuid + '), RSSI ' + details.rssi);
 });
